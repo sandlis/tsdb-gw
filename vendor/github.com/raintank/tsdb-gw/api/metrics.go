0371259937767b026083dc9b46c3ebe8a78eb122
Dieter Plaetinck <dieter@grafana.com>
2017-06-02 15:43:50 +0300

    track all dependencies with govendor
    1) ensure consistent builds in all environments,
       ensuring that bugs can't sneak in unknowingly due to dep changes
    2) make tsdbgw go-gettable
    3) make builds faster, no need to pull in deps each build
    
    This uses the latest master code for every dependency.

---
+++ vendor/github.com/raintank/tsdb-gw/api/metrics.go
@@ -0,0 +1,168 @@
+package api
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+
+	"github.com/golang/snappy"
+	"github.com/raintank/metrictank/stats"
+	"github.com/raintank/tsdb-gw/metric_publish"
+	"github.com/raintank/worldping-api/pkg/log"
+	"gopkg.in/raintank/schema.v1"
+	"gopkg.in/raintank/schema.v1/msg"
+)
+
+var (
+	metricsValid    = stats.NewCounter32("metrics.valid")
+	metricsRejected = stats.NewCounter32("metrics.rejected")
+)
+
+func Metrics(ctx *Context) {
+	contentType := ctx.Req.Header.Get("Content-Type")
+	switch contentType {
+	case "rt-metric-binary":
+		metricsBinary(ctx, false)
+	case "rt-metric-binary-snappy":
+		metricsBinary(ctx, true)
+	case "application/json":
+		metricsJson(ctx)
+	default:
+		ctx.JSON(400, fmt.Sprintf("unknown content-type: %s", contentType))
+	}
+}
+
+func metricsJson(ctx *Context) {
+	defer ctx.Req.Request.Body.Close()
+	if ctx.Req.Request.Body != nil {
+		body, err := ioutil.ReadAll(ctx.Req.Request.Body)
+		if err != nil {
+			log.Error(3, "unable to read request body. %s", err)
+		}
+		metrics := make([]*schema.MetricData, 0)
+		err = json.Unmarshal(body, &metrics)
+		if err != nil {
+			ctx.JSON(400, fmt.Sprintf("unable to parse request body. %s", err))
+			return
+		}
+
+		if ctx.IsAdmin {
+			for _, m := range metrics {
+				if m.Metric == "" {
+					m.Metric = m.Name
+				}
+				if m.Mtype == "" {
+					m.Mtype = "gauge"
+				}
+				if err := m.Validate(); err != nil {
+					metricsRejected.Add(len(metrics))
+					ctx.JSON(400, err.Error())
+					return
+				}
+			}
+		} else {
+			for _, m := range metrics {
+				m.OrgId = int(ctx.OrgId)
+				if m.Metric == "" {
+					m.Metric = m.Name
+				}
+				if m.Mtype == "" {
+					m.Mtype = "gauge"
+				}
+				if err := m.Validate(); err != nil {
+					metricsRejected.Add(len(metrics))
+					ctx.JSON(400, err.Error())
+					return
+				}
+				m.SetId()
+			}
+		}
+		metricsValid.Add(len(metrics))
+		err = metric_publish.Publish(metrics)
+		if err != nil {
+			log.Error(3, "failed to publish metrics. %s", err)
+			ctx.JSON(500, err)
+			return
+		}
+		ctx.JSON(200, "ok")
+		return
+	}
+	ctx.JSON(400, "no data included in request.")
+}
+
+func metricsBinary(ctx *Context, compressed bool) {
+	var body io.ReadCloser
+	if compressed {
+		body = ioutil.NopCloser(snappy.NewReader(ctx.Req.Request.Body))
+	} else {
+		body = ctx.Req.Request.Body
+	}
+	defer body.Close()
+
+	if ctx.Req.Request.Body != nil {
+		body, err := ioutil.ReadAll(body)
+		if err != nil {
+			log.Error(3, "unable to read request body. %s", err)
+			ctx.JSON(500, err)
+			return
+		}
+		metricData := new(msg.MetricData)
+		err = metricData.InitFromMsg(body)
+		if err != nil {
+			log.Error(3, "payload not metricData. %s", err)
+			ctx.JSON(500, err)
+			return
+		}
+
+		err = metricData.DecodeMetricData()
+		if err != nil {
+			log.Error(3, "failed to unmarshal metricData. %s", err)
+			ctx.JSON(500, err)
+			return
+		}
+
+		if ctx.IsAdmin {
+			for _, m := range metricData.Metrics {
+				if m.Metric == "" {
+					m.Metric = m.Name
+				}
+				if m.Mtype == "" {
+					m.Mtype = "gauge"
+				}
+
+				if err := m.Validate(); err != nil {
+					metricsRejected.Add(len(metricData.Metrics))
+					ctx.JSON(400, err.Error())
+					return
+				}
+			}
+		} else {
+			for _, m := range metricData.Metrics {
+				m.OrgId = int(ctx.OrgId)
+				if m.Metric == "" {
+					m.Metric = m.Name
+				}
+				if m.Mtype == "" {
+					m.Mtype = "gauge"
+				}
+				if err := m.Validate(); err != nil {
+					metricsRejected.Add(len(metricData.Metrics))
+					ctx.JSON(400, err.Error())
+					return
+				}
+				m.SetId()
+			}
+		}
+		metricsValid.Add(len(metricData.Metrics))
+		err = metric_publish.Publish(metricData.Metrics)
+		if err != nil {
+			log.Error(3, "failed to publish metrics. %s", err)
+			ctx.JSON(500, err)
+			return
+		}
+		ctx.JSON(200, "ok")
+		return
+	}
+	ctx.JSON(400, "no data included in request.")
+}
