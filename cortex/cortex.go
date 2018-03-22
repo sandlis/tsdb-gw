cccc53988c65a7223c2a9a0eb343b6b56a8e080b
Jacob Lisi <jacob.t.lisi@gmail.com>
2018-03-22 11:45:15 -0400

    add buffer pool to cortex remote write

--- cortex/cortex.go
+++ cortex/cortex.go
@@ -5,11 +5,16 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+
+	"github.com/oxtoacart/bpool"
 )
 
 var (
 	cortexWriteURL = flag.String("cortex-write-url", "http://localhost:9000", "cortex write address")
 	cortexReadURL  = flag.String("cortex-read-url", "http://localhost:9000", "cortex read address")
+
+	cortexWriteBPoolSize  = flag.Int("cortex-bpool-size", 100, "max number of byte buffers in the cortex write buffer pool")
+	cortexWriteBPoolWidth = flag.Int("cortex-bpool-width", 1024, "capacity of byte array provided by cortex write buffer pool")
 
 	// Proxy handles all non write requests to cortex
 	Proxy *httputil.ReverseProxy
@@ -37,7 +42,7 @@ func Init() error {
 			req.URL.Scheme = CortexWriteURL.Scheme
 			req.URL.Host = CortexWriteURL.Host
 		},
-		// BufferPool: (add BufferPool for perf reasons if needed)
+		BufferPool: bpool.NewBytePool(*cortexWriteBPoolSize, *cortexWriteBPoolWidth),
 	}
 	return nil
 }
