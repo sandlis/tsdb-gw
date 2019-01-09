b4ad683c9945a555872c5c54a1e16f29348e8a35
Dieter Plaetinck <dieter@grafana.com>
2019-01-09 13:24:13 +0100

    collect no more than 10 examples per error

--- ingest/metrics_response.go
+++ ingest/metrics_response.go
@@ -17,11 +17,16 @@ type ValidationError struct {
 	ExampleIds []int
 }
 
+// AddInvalid updates the counts and makes sure there's up to 10 examples per error
 func (m *MetricsResponse) AddInvalid(err error, index int) {
 	key := err.Error()
 	m.Invalid++
 	vErr := m.ValidationErrors[key]
 	vErr.Count++
-	vErr.ExampleIds = append(vErr.ExampleIds, index)
+
+	if vErr.Count < 10 {
+		vErr.ExampleIds = append(vErr.ExampleIds, index)
+	}
+
 	m.ValidationErrors[key] = vErr
 }
